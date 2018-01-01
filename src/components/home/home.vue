d7c772de05a0d67e19ee728174db0afe52592ffd
yuanxiaoyu <619019728@qq.com>
2018-01-01 14:50:58 +0800

    首页完成

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -7,6 +7,35 @@
         <img :src="item.img" alt="item.url">
       </mt-swipe-item>          
     </mt-swipe>
+
+    <!-- 九宫格菜单 -->
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <router-link to="/home/newslist">
+          <span class="mui-icon mui-icon-1"></span>
+          <div class="mui-media-body">新闻咨询</div>
+        </router-link>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <router-link to="/home/photolist">
+              <span class="mui-icon mui-icon-2"></span>
+              <div class="mui-media-body">图片分享</div>
+        </router-link>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-3"></span>
+              <div class="mui-media-body">商品购买</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-4"></span>
+              <div class="mui-media-body">留言反馈</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-5"></span>
+              <div class="mui-media-body">视频专区</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-6"></span>
+              <div class="mui-media-body">联系我们</div></a></li>
+    </ul>
+
   </div>
 </template>
 <script>
@@ -24,7 +53,7 @@ export default {
     this.$http
     .get('http://vue.studyit.io/api/getlunbo')
     .then(res => {
-      console.log(res)
+      // console.log(res)
       
       const data = res.data
       if(data.status === 0){
@@ -38,7 +67,8 @@ export default {
 
 </script>
 
-<style>
+<style lang="scss">
+
 .mint-swipe {
     height: 180px;
   }
@@ -46,5 +76,44 @@ export default {
   .mint-swipe-item img {
     width: 100%;
   }
+
+  .mui-table-view-cell .mui-media {
+    border: 0;    
+  } 
+
+  .mui-grid-view.mui-grid-9 {
+    background-color: #fff;
+    border: 0;
+      .mui-tabel-view-cell {
+      border: 0;
+    }
+   
+  }
+  
+  
+  .mui-icon {
+    height: 60px;
+    width: 60px;
+    background-size: cover;
+  }
+  .mui-icon-1 {
+    background-image: url(../../assets/images/menu1.png);
+  }
+  .mui-icon-2 {
+    background-image: url(../../assets/images/menu2.png);
+  }
+  .mui-icon-3 {
+    background-image: url(../../assets/images/menu3.png);
+  }
+  .mui-icon-4 {
+    background-image: url(../../assets/images/menu4.png);
+  }
+  .mui-icon-5 {
+    background-image: url(../../assets/images/menu5.png);
+  }
+  .mui-icon-6 {
+    background-image: url(../../assets/images/menu6.png);
+  }
+  
 
 </style>
